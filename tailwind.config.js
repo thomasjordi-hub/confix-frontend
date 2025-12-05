9ce35d0fdfeb847b848e2f3de1939c00af53ddb8
thomasjordi-hub <thomas.jordi@gmx.net>
2025-12-06 00:20:06 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      white: "#ffffff",
+      black: "#000000",
+      gray: {
+        50: "#f9fafb",
+        100: "#f3f4f6",
+        200: "#e5e7eb",
+        300: "#d1d5db",
+        400: "#9ca3af",
+        500: "#6b7280",
+        600: "#4b5563",
+        700: "#374151",
+        800: "#1f2937",
+        900: "#111827",
+      },
+      red: {
+        50: "#fef2f2",
+        100: "#fee2e2",
+        200: "#fecaca",
+        300: "#fca5a5",
+        400: "#f87171",
+        500: "#ef4444",
+        600: "#dc2626",
+        700: "#b91c1c",
+      },
+      yellow: {
+        500: "#eab308",
+      },
+      green: {
+        600: "#16a34a",
+      },
+      blue: {
+        600: "#2563eb",
+        700: "#1d4ed8",
+      },
+    },
+  },
+  plugins: [],
+};
